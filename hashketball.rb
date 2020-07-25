2d3d785880a4a0eb0b2819b7591ea378b1ab9607
sydneygold <sydney.goldberg@outlook.com>
2020-07-25 16:55:29 -0600

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-# Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -126,4 +126,129 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(name)
+
+  stats = game_hash
+
+  stats.each do |team, team_data|
+
+  team_data[:players].each do |player_name|
+
+if player_name[:player_name] == name
+
+   return player_name[:points]
+
+      end
+
+      #binding.pry
+
+    end
+
+  end
+
+end
+
+ 
+
+def shoe_size(name)
+
+  stats = game_hash
+
+  stats.each do |team, team_data|
+
+  team_data[:players].each do |player_name|
+
+if player_name[:player_name] == name
+
+   return player_name[:shoe]
+
+      end
+
+    end
+
+  end
+
+end
+
+ 
+
+def team_colors(team_name)
+
+  stats = game_hash
+
+  stats.each do |team, team_data|
+
+if team_data[:team_name] == team_name
+
+   return team_data[:colors]
+
+    end
+
+  end
+
+end
+
+ 
+
+def team_names()
+
+  team = []
+
+  stats = game_hash
+
+  stats.map do |team, team_data|
+
+    team_data[:team_name]
+
+    end
+
+end
+
+ 
+
+def player_numbers(team_name)
+
+  stats = game_hash
+  team_numbers = []
+  
+  stats.map do |number, team_data|
+   if team_data[:team_name] == team_name 
+    team_data[:players].map do |key|
+    team_numbers << key[:number]
+    end
+   end
+   
+  end
+  team_numbers
+end
+
+ 
+
+def player_stats(name)
+stats = game_hash
+  stats.map do |team, team_data|
+    team_data[:players].map do |player_name|
+    if player_name[:player_name] == name
+      return player_name
+ 
+      end  
+    end
+  end
+end
+
+ 
+
+def big_shoe_rebounds()
+stats = game_hash
+shoe_size = 0
+rebounds = 0
+stats.each do |team, team_data|
+  team_data[:players].each do |players_shoe|
+    if players_shoe[:shoe] > shoe_size 
+      shoe_size = players_shoe[:shoe]
+      rebounds = players_shoe[:rebounds] 
+      end
+    end 
+  end
+  rebounds
+end
